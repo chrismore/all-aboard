4d97d1f6906537836e63386c9b8c243ff7288c4b
schalkneethling <sneethling@mozilla.com>
2016-09-13 14:38:05 +0200

    Adds functional tests for successful values flow

--- tests-functional/pages/sidebar.py
+++ tests-functional/pages/sidebar.py
@@ -13,7 +13,33 @@ class Sidebar(Page):
             var cta = document.querySelector('#sidebar').contentDocument
                               .querySelector('#web-panels-browser').contentDocument
                               .querySelector('main button');
-            return cta.dataset['step'];
+            return cta.dataset.step;
+        """)
+
+    @property
+    def content_container_id(self):
+        return self.selenium.execute_script("""
+            var container = document.querySelector('#sidebar').contentDocument
+                              .querySelector('#web-panels-browser').contentDocument
+                              .querySelector('body > section');
+            return container.getAttribute('id');
+        """)
+
+    @property
+    def prize_button(self):
+        return self.selenium.execute_script("""
+            var button = document.querySelector('#sidebar').contentDocument
+                              .querySelector('#web-panels-browser').contentDocument
+                              .querySelector('footer #prize');
+            return button;
+        """)
+
+    def claim_prize(self):
+        return self.selenium.execute_script("""
+            var button = document.querySelector('#sidebar').contentDocument
+                              .querySelector('#web-panels-browser').contentDocument
+                              .querySelector('footer #prize');
+            button.click();
         """)
 
     def click_cta(self):
---
+++ tests-functional/tests/test_positive_values_user_flow.py
@@ -0,0 +1,82 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as expected
+from selenium.webdriver.support.ui import WebDriverWait as Wait
+
+from pages.all_aboard import AllAboard
+from pages.firstrun import Firstrun
+from pages.sidebar import Sidebar
+
+
+@pytest.fixture
+def complete_firstrun(base_url, selenium):
+    """ Completes /firstrun interactions to start on-boarding"""
+    page = Firstrun(selenium, base_url).open()
+    page.complete_firstrun_values_flow()
+    return page
+
+
+@pytest.mark.nondestructive
+@pytest.mark.capabilities(firefox_args=['-foreground'])
+def test_successful_values_flow(base_url,
+                                selenium,
+                                complete_firstrun):
+    """Tests that a user can successfully complete the values track"""
+    # switch to browser chrome
+    selenium.set_context('chrome')
+
+    all_aboard = AllAboard(selenium)
+    sidebar = Sidebar(selenium)
+
+    # ensure that add-on action button is added as soon as we leave firstrun
+    Wait(selenium, 2).until(
+        expected.presence_of_element_located(
+            (By.ID, 'action-button--all-aboard-v1-all-aboard')
+        )
+    )
+
+    # enure that the sidebar is automatically shown on first notification
+    Wait(selenium, 3).until(
+        expected.presence_of_element_located((
+            By.CSS_SELECTOR,
+            '#content-deck #sidebar-box[sidebarcommand="viewWebPanelsSidebar"]'
+            )
+        )
+    )
+
+    previous_step = 0
+    while int(sidebar.current_step) < 5:
+        # wait for the next notification to happen
+        notification = Wait(selenium, 15).until(
+            expected.presence_of_element_located((
+                By.CSS_SELECTOR,
+                '#action-button--all-aboard-v1-all-aboard[badge="1"]'
+                )
+            )
+        )
+
+        # once the content notification happens, click the action button
+        notification.click()
+
+        # ensure that the sidebar is shown after click on ActionButton
+        assert all_aboard.sidebar.get_attribute('hidden') == 'false'
+        # ensure that the correct sidebar is shown
+        assert int(sidebar.current_step) == previous_step + 1
+
+        # store the current step for the next iteration
+        previous_step = int(sidebar.current_step)
+        # move the experience on to the next step
+        sidebar.click_cta()
+
+    # click the claim prize button
+    sidebar.claim_prize()
+
+    # ensure that the reward sidebar is shown
+    Wait(selenium, 3).until(
+        lambda s: sidebar.content_container_id == 'reward'
+    )
